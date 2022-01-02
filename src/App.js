da459977b9212584fcfc1601bd5a905f39391718
vinay kashyap <vinaykashyap@MBP-von-vinay.fritz.box>
2022-01-02 02:59:54 +0100

    auth page and context and basic design

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import './App.css';
-import { BrowserRouter , Route , Routes , Navigate} from "react-router-dom"
-import AuthPage from './pages/Auth';
-import BookingsPage from './pages/Bookings';
-import EventsPage from './pages/Events';
-import MainNavigation from './components/NavBar/MainNavigation';
+import React, { useState } from "react";
+
+import "./App.css";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import AuthPage from "./pages/Auth";
+import BookingsPage from "./pages/Bookings";
+import EventsPage from "./pages/Events";
+import MainNavigation from "./components/NavBar/MainNavigation";
+import AuthContext from "./context/auth-context";
 
 function App() {
+  const [token, setToken] = useState(null);
+  const [userid, setUserId] = useState(null);
+
+  const login = (token, tokenExpiration, userid) => {
+    setToken(token);
+    setUserId(userid);
+  };
+  const logout = () => {
+    setToken(null);
+    setUserId(null);
+  };
+
   return (
     <BrowserRouter>
-    <MainNavigation />
-    <main className='main-content'>
-    <Routes>
-      <Route path="/" element={<Navigate to="/events" />} />
-      <Route path="/auth" element={<AuthPage />} />
-      <Route path="/events" element={<EventsPage />} />
-      <Route path="/booking" element={<BookingsPage />}/>
-    </Routes>
-    </main>
+      <AuthContext.Provider
+        value={{ token: token, userid: userid, login: login, logout: logout }}
+      >
+        <MainNavigation />
+        <main className="main-content">
+          <Routes>
+            {!token && <Route path="/" element={<Navigate to="/auth" />} />}
+            {token && <Route path="/" element={<Navigate to="/events" />} />}
+            {token && (
+              <Route path="/auth" element={<Navigate to="/events" />} />
+            )}
+
+            {!token && <Route path="/auth" element={<AuthPage />} />}
+            <Route path="/events" element={<EventsPage />} />
+            {token && <Route path="/booking" element={<BookingsPage />} />}
+          </Routes>
+        </main>
+      </AuthContext.Provider>
     </BrowserRouter>
   );
 }
--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,11 +1,101 @@
-import React,{ Component } from "react";
+import React, { Component } from "react";
+import "./Auth.css";
+import AuthContext from "../context/auth-context";
 
-class AuthPage extends Component{
-    render() {
-        return (
-            <h1>This is the Home page</h1>
-        );
+class AuthPage extends Component {
+  state = {
+    isLogin: true,
+  };
+
+  static contextType = AuthContext;
+
+  constructor(props) {
+    super(props);
+    this.emailEl = React.createRef();
+    this.passwordEl = React.createRef();
+  }
+  switchModeHandler = (event) => {
+    event.preventDefault();
+    this.setState((prevState) => {
+      return { isLogin: !prevState.isLogin };
+    });
+  };
+  submitHandler = (event) => {
+    event.preventDefault();
+    const email = this.emailEl.current.value;
+    const password = this.passwordEl.current.value;
+
+    if (email.trim().length === 0 || password.trim().length === 0) {
+      return;
     }
+    let requestBody = {
+      query: `
+            query{
+                login(email:"${email}",password:"${password}"){
+                    userid
+                    Token
+                    TokenExpiration
+                }
+            }
+        `,
+    };
+    if (!this.state.isLogin) {
+      requestBody = {
+        query: `
+              mutation{
+                  createUser(userInput : {email:"${email}" , password: "${password}" }) {
+                      _id
+                      email
+                  }
+              } `,
+      };
+    }
+    fetch(process.env.REACT_APP_BASE_URL, {
+      method: "POST",
+      body: JSON.stringify(requestBody),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error("Failed!");
+        }
+        return res.json();
+      })
+      .then((resData) => {
+        if (resData.data.login.Token) {
+          this.context.login(
+            resData.data.login.Token,
+            resData.data.login.userid,
+            resData.data.login.TokenExpiration
+          );
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  render() {
+    return (
+      <form className="auth-form" onSubmit={this.submitHandler}>
+        <div className="form-control">
+          <label htmlFor="email">E-mail</label>
+          <input type="email" id="email" ref={this.emailEl}></input>
+        </div>
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" ref={this.passwordEl}></input>
+        </div>
+        <div className="form-actions">
+          <button type="submit">Submit</button>
+          <button type="button" onClick={this.switchModeHandler}>
+            Switch to {this.state.isLogin ? "Signup" : "Login"}
+          </button>
+        </div>
+      </form>
+    );
+  }
 }
 
 export default AuthPage;
